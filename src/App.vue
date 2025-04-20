bca33565b4d9890fdb0eb01846221835e9398464
Sasa Nedic <svetasmak0@gmail.com>
2025-04-20 08:44:30 +0200

    Pass data via props

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
-<script setup>
+<template>
+  <div id="app">
+    <Task :tasks="tasks"></Task>
+  </div>
+</template>
+
+<script>
 import Task from './components/Task.vue';
 
 
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Learn Vue JS",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Watch netflix",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Go shopping",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Learn guitar",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Send email",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
 </script>
-
-<template>
-<Task></Task>
-</template>
-
-<style scoped>
-
-</style>
